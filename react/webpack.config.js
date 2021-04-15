d7f86e176d29c0857999be2b007b26591cce3d98
cordelia <cordeliasixth@gmail.com>
2021-04-15 12:50:26 +0900

    feat: add html-webpack-plugin closes #30

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,13 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, "src/index.jsx"),
     output: {
       filename: 'bundle.js',
-      path: path.join(__dirname, "dist")
+      path: path.join(__dirname, "dist"),
+      // ブラウザ更新に必要
+      publicPath: "/",
+
     },
     devServer: {
+        historyApiFallback: true,
         compress: true,
         contentBase: path.join(__dirname, "dist"),
         watchContentBase: true,
@@ -33,5 +38,10 @@ module.exports = {
     devtool: 'inline-source-map',
     resolve: {
       extensions: ['.js', '.jsx']
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: __dirname + '/src/index.html',
+      })
+    ]
 };
